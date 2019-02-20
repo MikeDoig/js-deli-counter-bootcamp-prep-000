daf98846ea14af8998123914735a6b5b99d5b560
Mike Doig <mike.j.doig@gmail.com>
2019-02-20 19:17:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(current) {
   else {
   const positions = [];
   for (let i=0; i<current.length; i++){
-    positions.push(`${i}. ${current[i]}`);
+    positions.push(`${i}. ${current[i + 1]}`);
   }
   return `The line is currently: ${positions.join(', ')}`;
 }
