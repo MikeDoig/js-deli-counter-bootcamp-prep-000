7f60715ff6844edcb6ea5a5a6a69d95f9c5e4181
Mike Doig <mike.j.doig@gmail.com>
2019-02-20 20:12:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ function takeANumber (line, name) {
   return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
+function takeANumberTwo (line, name)
+  
+
 function nowServing(front) {
   if (front.length===0)
   return 'There is nobody waiting to be served!';
@@ -16,7 +19,7 @@ function currentLine(current) {
   if (current.length===0) {
   return 'The line is currently empty.';
   }
-  
+
   else {
   const positions = [];
   for (let i=0; i<current.length; i++){
