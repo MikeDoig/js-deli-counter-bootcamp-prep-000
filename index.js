35317f3676d75fe976ceeb7350315954db928622
Mike Doig <mike.j.doig@gmail.com>
2019-02-20 19:08:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function nowServing(front) {
   return `Currently serving ${front.shift()}.`;
  }
  
-function currentLine(currents) {
+function currentLine(current) {
   if (current.length===0) {
   return 'The line is currently empty.';
   }
